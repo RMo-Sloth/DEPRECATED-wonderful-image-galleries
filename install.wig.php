32705d2af1c19d4b7a0c4a45cb46e7651a7172c0
Sloth <robmonhemius@gmail.com>
2018-05-16 00:05:41 +0200

    License update #1

--- install.wig.php
+++ install.wig.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @package Module Wonderful Image Galleries
+ * @author RMo
+ * @copyright (C) 2018 - RMo
+ * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+**/
+
 // No direct access to this file
 defined('_JEXEC') or die;
 
