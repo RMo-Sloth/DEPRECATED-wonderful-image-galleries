2b84f8a96c283c57aedfa930c5d44a4c31066110
Sloth <robmonhemius@gmail.com>
2018-05-16 00:19:49 +0200

    Update license (9)

--- tmpl/svg/animations/animate-circles.php
+++ tmpl/svg/animations/animate-circles.php
@@ -1,4 +1,12 @@
-<?php defined('_JEXEC') or die; ?>
+<?php 
+	/**
+	 * @package Module Wonderful Image Galleries
+	 * @author RMo
+	 * @copyright (C) 2018 - RMo
+	 * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+	**/
+	defined('_JEXEC') or die; 
+?>
 <mask id="clip2" width="2550" height="1925" x="0" y="0">
 	<rect width="0" height="1925" fill="black"></rect>
 	<circle id="clip2_circle1" cx="1000" cy="1000" r="0" fill="white"></circle>
