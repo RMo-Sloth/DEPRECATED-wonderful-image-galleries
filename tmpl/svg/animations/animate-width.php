73d321bd149ad39400b6d36ce555553dbff061a7
Sloth <robmonhemius@gmail.com>
2018-05-16 00:21:45 +0200

    Update license (10)

--- tmpl/svg/animations/animate-width.php
+++ tmpl/svg/animations/animate-width.php
@@ -1,4 +1,12 @@
-<?php defined('_JEXEC') or die; ?>
+<?php 
+	/**
+	 * @package Module Wonderful Image Galleries
+	 * @author RMo
+	 * @copyright (C) 2018 - RMo
+	 * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+	**/
+	defined('_JEXEC') or die; 
+?>
 <mask id="clip1">
 	<rect id="clip1_rect" width="0" height="100%" fill="white"></rect>
 </mask>
