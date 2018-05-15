32bf41896b00d2586036f0a4f51e7d980580abfa
Sloth <robmonhemius@gmail.com>
2018-05-16 00:13:52 +0200

    Update license (6)

--- tmpl/js/animations/animate-width.php
+++ tmpl/js/animations/animate-width.php
@@ -1,4 +1,12 @@
-<?php defined('_JEXEC') or die; ?>
+<?php 
+/**
+ * @package Module Wonderful Image Galleries
+ * @author RMo
+ * @copyright (C) 2018 - RMo
+ * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+**/
+defined('_JEXEC') or die; 
+?>
 function(){
 	let thisWonderfulImageGalleryObjectMainImage = this;
 	let duration = this.animationDuration;
