f724ed719448fb29544e20a7a483ac049d83fb47
Sloth <robmonhemius@gmail.com>
2018-05-16 00:06:55 +0200

    license update #2

--- mod_wig.php
+++ mod_wig.php
@@ -1,4 +1,11 @@
 <?php 
+	/**
+	 * @package Module Wonderful Image Galleries
+	 * @author RMo
+	 * @copyright (C) 2018 - RMo
+	 * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+	**/
+	
 	defined('_JEXEC') or die;
 	require JModuleHelper::getLayoutPath('mod_wig');
 ?>
