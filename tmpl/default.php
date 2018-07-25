92ce1589c38817091a960f40b0d4132f9eb2fd18
RMo-Sloth <robmonhemius@gmail.com>
2018-07-25 16:37:19 +0200

    Refactored css to OOP

--- tmpl/default.php
+++ tmpl/default.php
@@ -1,15 +1,20 @@
-<?php 
+<?php
 /**
  * @package Module Wonderful Image Galleries
  * @author RMo
  * @copyright (C) 2018 - RMo
  * @license GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
 **/
-	defined('_JEXEC') or die; 
-?>
-<?php
+	defined('_JEXEC') or die;
+
 	// compile the module
-	require JModuleHelper::getLayoutPath('mod_wig', 'css/css');
+	require JModuleHelper::getLayoutPath('mod_wig', 'lib/animation');
+	$animation = new testAnimation(
+		$module->id,
+		$params->get('images-background-color'),
+		$params->get('background-color')
+	);
+	$animation->css();
+
 	require JModuleHelper::getLayoutPath('mod_wig', 'svg/svg');
 	require JModuleHelper::getLayoutPath('mod_wig', 'js/js');
-?>
---
+++ tmpl/lib/animation.php
@@ -0,0 +1,59 @@
+<?php
+// No direct access
+// defined('_JEXEC') or die;
+
+abstract class Animation{
+  protected $index;
+  protected $imagesBackgroundColor;
+  protected $backgroundColor;
+
+  public function __construct( $id, $imagesBackgroundColor, $backgroundColor ){
+    $this->index = $id;
+    $this->imagesBackgroundColor = $imagesBackgroundColor;
+    $this->backgroundColor = $backgroundColor;
+  }
+  public function css(){
+    // css styling of svg for easy theming
+    echo "<style>";
+
+    echo "#background-rect$this->index, #new-rect$this->index{";
+    echo "  fill: $this->imagesBackgroundColor;";
+    echo "}";
+
+    // background and border of slides and the main image
+    echo "#wonderful-image-gallery-$this->index .slides>rect{";
+    echo "  fill: $this->imagesBackgroundColor;";
+    echo "  stroke: $this->backgroundColor;";
+    echo "}";
+
+    // default svg styling (should not change)
+    echo "#wonderful-image-gallery-$this->index .slides>rect, #more-pictures-left$this->index, #more-pictures-right$this->index, #wonderful-image-gallery-$this->index .play-button, #wonderful-image-gallery-$this->index .main-images{";
+    echo "cursor: pointer;";
+    echo "}";
+
+    echo "#wonderful-image-gallery-$this->index .seen-slide{";
+    echo "  filter:url(#sepia);";
+    echo "}";
+
+    echo "#wonderful-image-gallery-$this->index .slides>rect.active-slide{";
+    echo "	stroke: $this->backgroundColor";
+    echo "}";
+
+    echo "#wonderful-image-gallery-$this->index .slides>image{";
+    echo '  pointer-events: none;';
+    echo '}';
+
+    echo "#wonderful-image-gallery-$this->index .leftNav{";
+    echo '  display: none;';
+    echo '}';
+
+    echo "</style>";
+  }
+  protected abstract function get_svg();
+  protected abstract function get_js();
+}
+
+class testAnimation extends Animation{
+  protected function get_svg(){}
+  protected function get_js(){}
+}
